fa08fef3736283d531b0cfaea52a97fccf2024bb
Dan <dan.soft.studio@gmail.com>
2025-01-03 12:39:39 +0700

    update

--- CommonUtils.pas
+++ CommonUtils.pas
@@ -1604,6 +1604,10 @@ generic function UEndianSwap<T>(const v: T): T; inline; overload;
 function UEndianSwap(const v: UInt16): UInt16; inline; overload;
 function UEndianSwap(const v: UInt32): UInt32; inline; overload;
 function UEndianSwap(const v: UInt64): UInt64; inline; overload;
+generic procedure UByteSwap(var v: T); inline; overload;
+procedure UByteSwap(var v: UInt16); inline; overload;
+procedure UByteSwap(var v: UInt32); inline; overload;
+procedure UByteSwap(var v: UInt64); inline; overload;
 generic procedure USwap<T>(var a: T; var b: T); inline; overload;
 procedure USwap(var a: Int8; var b: Int8); inline; overload;
 procedure USwap(var a: Int16; var b: Int16); inline; overload;
@@ -8685,6 +8689,36 @@ end;
 function UEndianSwap(const v: UInt64): UInt64;
 begin
   Result := specialize UEndianSwap<UInt64>(v);
+end;
+
+generic procedure UByteSwap(var v: T);
+  type TByteArr = array[0..SizeOf(T) - 1] of UInt8;
+  var Arr: TByteArr absolute v;
+  var Tmp: UInt8;
+  var i, j: Int32;
+begin
+  for i := 0 to (Length(Arr) shr 1) - 1 do
+  begin
+    j := High(Arr) - i;
+    Tmp := Arr[i];
+    Arr[i] := Arr[j];
+    Arr[j] := Tmp;
+  end;
+end;
+
+procedure UByteSwap(var v: UInt16);
+begin
+  specialize UByteSwap<UInt16>(v);
+end;
+
+procedure UByteSwap(var v: UInt32);
+begin
+  specialize UByteSwap<UInt32>(v);
+end;
+
+procedure UByteSwap(var v: UInt64);
+begin
+  specialize UByteSwap<UInt64>(v);
 end;
 
 generic procedure USwap<T>(var a: T; var b: T);
