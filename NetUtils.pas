fcfb5b1c0e3c0d8301376155bc3d81a66ccd62de
Dan <dan.soft.studio@gmail.com>
2025-08-19 22:04:50 +0700

    update

--- NetUtils.pas
+++ NetUtils.pas
@@ -511,6 +511,7 @@ public
       TimeStamp: UInt64;
       Name: String;
       Message: String;
+      class operator = (const a, b: TPeer): Boolean;
     end;
     type TPeerArray = array of TPeer;
   private
@@ -1140,6 +1141,11 @@ class function TUSocketImpl.CreateUDP(
 begin
   Result := TUSocket.Invalid;
   Result.MakeUDP(SockDomain, SockProtocol);
+end;
+
+class operator TUNet.TBeacon.TPeer.=(const a, b: TPeer): Boolean;
+begin
+  Result := a.Addr.Addr32 = b.Addr.Addr32;
 end;
 
 procedure TUNet.TBeacon.SetEnabled(const Value: Boolean);
